cbd531fc36a31933f266c259c9ba512498c6bce7
Ali Biner <102030146+AliBiner@users.noreply.github.com>
2024-02-09 12:59:21 +0300

    Create DataJPA project

---
+++ 3-DataJPAWithPostgre/src/main/java/com/example/dataJPAWithPostgre/DataJpaWithPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataJPAWithPostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaWithPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataJpaWithPostgreApplication.class, args);
+	}
+
+}
